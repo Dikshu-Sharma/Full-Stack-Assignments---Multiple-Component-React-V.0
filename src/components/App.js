a00d8654d9c70e2d5adcac430b214acd7a804ed1
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-10-24 13:21:04 +0530

    lalalalalalla

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div value="project-name">CHESS GAMBLING WEBSITE</div>
+        <div value="project-description">
+          Its a GAMBLING WEBSITE YOU CAN BET ON GAME AND WIN IT AND GET THE
+          MONEY
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
